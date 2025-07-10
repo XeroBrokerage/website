4ab3fe0bd2073055648bfb34690c82f25d1427e1
umair-moon-malik <umairmalik0@proton.me>
2025-07-10 18:14:47 +0530

    fix: logo in small devices

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -35,13 +35,13 @@ export default function Navbar() {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 -960 960 960"
               fill="#000000"
-              className="sm:w-[50px] sm:h-[50px] w-[30px] h-[30px]"
+              className="sm:w-[50px] sm:h-[50px] w-[33px] h-[33px]"
             >
               <path d="M370-440h60v-120h100v120h60v-185l-110-73-110 73v185Zm110 281q133-121 196.5-219.5T740-552q0-118-75.5-193T480-820q-109 0-184.5 75T220-552q0 75 65 173.5T480-159Zm0 79Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
             </svg>
           </span>
           <div className="flex flex-col justify-center">
-            <span className="text-xl sm:text-3xl poppins-bold tracking-widest -mb-2">
+            <span className="text-xl sm:text-3xl poppins-bold tracking-widest mb-[-5px] sm:-mb-2">
               xero
             </span>
             <span className="text-[10px] sm:text-lg poppins-regular tracking-widest">
